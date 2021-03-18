22102fbd01098d31c8c7f72995af6ca35f34fd3b
Sergey Lyashko <slyashko@mail.ru>
2021-03-18 19:19:23 +0300

    add tests & manifest

--- src/test/java/handlers/UnpackingImplTest.java
+++ src/test/java/handlers/UnpackingImplTest.java
@@ -1,13 +1,11 @@
 package handlers;
 
-import configuration.UnpackingStringConfiguration;
 import exceptions.NoCloseStringPackException;
 import exceptions.NoOpenStringPackException;
 import exceptions.NoSuchSizePackingException;
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class UnpackingImplTest {
@@ -20,9 +18,25 @@ public class UnpackingImplTest {
 
     @Test
     public void unpack_valid_string() throws NoSuchSizePackingException, NoOpenStringPackException, NoCloseStringPackException {
-        UnpackingImpl unpackingSpy = spy(new UnpackingImpl());
+        UnpackingImpl unpackingMock = spy(new UnpackingImpl());
         String in = "2[3[xz]y]k";
         String out = "xzxzxzyxzxzxzyk";
-        when(unpackingSpy.unpack(in)).thenReturn(out);
+        assertEquals(unpackingMock.unpack(in), out);
+    }
+
+    @Test
+    public void unpack_invalid_string() throws NoCloseStringPackException, NoSuchSizePackingException, NoOpenStringPackException {
+        UnpackingImpl unpackingMock = spy(new UnpackingImpl());
+        String in = "2[test]";
+        assertNotEquals(unpackingMock.unpack(in), "test");
+    }
+
+    @Test
+    public void unpack_throw() throws NoCloseStringPackException, NoSuchSizePackingException, NoOpenStringPackException {
+        UnpackingImpl unpackingMock = spy(new UnpackingImpl());
+        String in = "test";
+        doThrow(new NoSuchSizePackingException()).when(unpackingMock).unpack(in);
+        doThrow(new NoCloseStringPackException()).when(unpackingMock).unpack(in);
+        doThrow(new NoOpenStringPackException()).when(unpackingMock).unpack(in);
     }
 }
